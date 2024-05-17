49103251e89747a729ab5a1dcb7644b1130406ae
patel9619ali <patel9619ali@gmail.com>
2024-05-17 19:36:55 +0530

    changes

--- src/StarRatings.js
+++ src/StarRatings.js
@@ -1,20 +1,32 @@
-import { React } from "react";
+import { React, useState } from "react";
 import Stars from "./Stars";
 
 export default function StarRatings(){
-    const numberOfStars = 10;
-    const starsArray = Array(numberOfStars).fill(null);
+    const [isHovered, setIsHovered] = useState(false);
+    const [hoveredIndex, setHoveredIndex] = useState(10);
+    // const numberOfStars = 10;
+    const starsArray = Array(hoveredIndex).fill(null);
+    // console.log(starsArray)
+    const handleMouseEnter = (index) => {
+        setHoveredIndex(index);
+      };
+      const handleMouseLeave = () => {
+        console.log("handleMouseLeave")
+        setHoveredIndex(null);
+      };
+    console.log("starsArray")
     return(
         <>
             <div className="stars_wrapper">
-                {starsArray.map((_, index) => (
-                    <Stars key={index} />
+                {starsArray.map((_, hoveredIndex) => (
+                    <div key={hoveredIndex} onMouseEnter={() => handleMouseEnter(hoveredIndex)} onMouseLeave={handleMouseLeave}>
+                        <Stars onMouseEnter={() => handleMouseEnter(hoveredIndex)} key={hoveredIndex} isHovered={isHovered} setIsHovered ={setIsHovered } starsArray={starsArray} onMouseLeave={handleMouseLeave}/>
+                    </div>
                 ))}
+                {hoveredIndex !== null && (
+  <span>Currently hovering over item at index: {hoveredIndex}</span>
+)}
             </div>
         </>
     )
 }
-
-{/*  Fill
-
- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#fcc419"><path d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" stroke-linecap="round" stroke-linejoin="round" stroke-width="{2}"></path></svg> */}
--- src/Stars.js
+++ src/Stars.js
@@ -1,22 +1,19 @@
-export default function StarRatings(){
-function handleHover(){
-    console.log('Hover')
-}
+import React, { useState } from 'react';
+export default function StarRatings({isHovered,setIsHovered}){
+    // console.log(isHovered,"In Stars Component")
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
     return(
-        <>
-          <span onHover={handleHover}><svg className="stars_height"
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="yellow"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="{2}"
-    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-  />
-</svg></span>
-        </>
+    <>
+        <span onMouseLeave={handleMouseLeave} onMouseEnter={handleMouseEnter}>
+            {isHovered?
+            <svg className="stars_height" xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 24 24" stroke="yellow"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
+            </svg>:<svg className="stars_height" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#fcc419"><path d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}"></path></svg>} 
+        </span>
+    </>
     )
 }
